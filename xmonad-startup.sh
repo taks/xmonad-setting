b7d4b2aca60827cf6d60c4754628434196c2b322
taks <857tn859@gmail.com>
2011-06-26 15:36:53 +0900

    add xmonad-startup.sh

---
+++ xmonad-startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## 起動するアプリケーション
+# icon trayの設定
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 20 &
+
+# スクリーンセーバの起動
+xscreensaver -no-splash &
+
+# clipit
+clipit &
+
+# deja-dup
+deja-dup-monitor &
+
+# xmodmap の設定
+xmodmap $HOME/.xmodmaprc
+
